62df21b1c80c2a0f7b2d2e116b3a96c6e6be853f
Fer <101359582+PVMarifer@users.noreply.github.com>
2025-04-15 00:29:46 -0600

    CRUD
    Se crea el crud de tareas

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,5 +1,22 @@
 import {Router} from 'express'
 import { authRequired } from '../middlewares/validateToken.js'
+import {getTask, getTasks, createTask, updateTask, deleteTask} from "../controllers/tasks.controller.js";
 const router = Router()
-router.get('/tasks', authRequired, (req, res) => res.send('tasks'))
+
+//RUTAS PARA EL CRUD 
+router.get('/tasks', authRequired, getTasks )
+router.get('/tasks/:id', authRequired,getTask ) //con un id especifico de tarea
+router.post('/tasks', authRequired, )
+router.delete('/tasks/:id', authRequired, )
+router.put('/tasks/:id', authRequired, )
+
 export default router
+
+/*
+1. Crear las rutas para el crud 
+2. Crear los tasks controller 
+3. Importar los tasks
+
+
+
+*/
