2db2a89dabc313c166d55626bdcbe3cd59c72e5a
Fer <101359582+PVMarifer@users.noreply.github.com>
2025-04-14 22:07:24 -0600

    Login validation - Login
    Se crea la validacion del login con un metodo isMatch para comparar las contraseñas creadas contra la que se usa para el login y permitir o negar el acceso
    
    El logout elimina- crea el token vacío

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,4 +32,36 @@ try {
 }
 };
 
-export const login = (req, res) => res.send('login');
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    
+ try {
+    const userFound = await User.findOne({email});
+    if (!userFound) return res.status(400).json({message: "Usuario no encontrado"});
+
+    const isMatch =  await bcrypt.compare(password, userFound.password);
+    if (!isMatch) return res.status(400).json({message: "Contraseña incorrecta"});
+
+    const token = await createAccessToken({id: userFound._id});
+        
+            res.cookie("token", token); //Se guarda en una cookie por defecto para que el frontend no tenga que leer todo el request body
+            res.json({
+            //Estos son los datos que vamos a extraer del frontend. El frontend no tiene que saber la contrasena del usuario
+            id: userFound._id,   //Trae el email de la constante userSaved donde se guardaron los documentos.
+            username: userFound.username,
+            email: userFound.email,
+        });
+        
+        
+ } catch (error) {
+     res.status(500).json({Message: error.Message});
+ }
+ };
+
+ export const logout = (req, res) => {
+    res.cookie('token', "", {
+        expires: new Date(0)
+    }); //Se resetea el token
+    return res.sendStatus(200);
+ };
+ 
